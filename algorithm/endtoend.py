df074863a3ca081d61dedc9b141d5d76d8aabca3
ceynri <ceynri@gmail.com>
2021-01-31 20:26:34 +0800

    initial commit

---
+++ algorithm/endtoend.py
@@ -0,0 +1,53 @@
+import numpy as np
+import os
+import torch
+import torchvision.models as models
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import sys
+import math
+import torch.nn.init as init
+import logging
+from torch.nn.parameter import Parameter
+from analysis import Analysis
+from synthesis import Synthesis
+from bit_estimate import BitEstimator
+
+
+class ImageCompressor(nn.Module):
+    def __init__(self, out_channel_N=128, training=True):
+        super(ImageCompressor, self).__init__()
+        self.Encoder = Analysis(out_channel_N=out_channel_N)
+        self.Decoder = Synthesis(out_channel_N=out_channel_N)
+        self.out_channel_N = out_channel_N
+        self.bitEstimator = BitEstimator(channel=out_channel_N)
+        #self.training = True
+        self.training = training
+
+    def forward(self, input_image):
+        quant_noise_feature = torch.zeros(input_image.size(0), self.out_channel_N, input_image.size(2) // 16, input_image.size(3) // 16).cuda()      
+        quant_noise_feature = torch.nn.init.uniform_(torch.zeros_like(quant_noise_feature), -0.5, 0.5)
+        feature = self.Encoder(input_image)
+        batch_size = feature.size()[0]
+        feature_renorm = feature
+
+        # quantization
+        if self.training:
+            compressed_feature_renorm = feature_renorm + quant_noise_feature
+        else:
+            compressed_feature_renorm = torch.round(feature_renorm)
+        recon_image = self.Decoder(compressed_feature_renorm)
+        
+        def iclr18_estimate_bits_z(z):
+            prob = self.bitEstimator(z + 0.5) - self.bitEstimator(z - 0.5)
+            total_bits = torch.sum(torch.clamp(-1.0 * torch.log(prob + 1e-10) / math.log(2.0), 0, 50))
+            return total_bits, prob
+        
+        # total_bits_feature, _ = iclr18_estimate_bits_z(compressed_feature_renorm)
+        # im_shape = input_image.size()
+        # bpp_feature = total_bits_feature / (batch_size * im_shape[2] * im_shape[3])                     
+        
+        return recon_image# , bpp_feature
---
+++ algorithm/synthesis.py
@@ -0,0 +1,25 @@
+
+import torch.nn as nn
+
+import torch
+import math
+from GDN import GDN 
+
+class Synthesis(nn.Module):
+    '''
+    Decode synthesis
+    '''
+
+    def __init__(self, out_channel_N=128):
+        super(Synthesis, self).__init__()
+        self.deconv1 = nn.ConvTranspose2d(out_channel_N, out_channel_N, 5, stride=2, padding=2, output_padding=1)
+        self.igdn1 = GDN(out_channel_N, inverse=True)
+        self.deconv2 = nn.ConvTranspose2d(out_channel_N, out_channel_N, 5, stride=2, padding=2, output_padding=1)
+        self.igdn2 = GDN(out_channel_N, inverse=True)
+        self.deconv3 = nn.ConvTranspose2d(out_channel_N, 3, 9, stride=4, padding=4, output_padding=3)
+
+    def forward(self, x):
+        x = self.igdn1(self.deconv1(x))
+        x = self.igdn2(self.deconv2(x))
+        x = self.deconv3(x)
+        return x
