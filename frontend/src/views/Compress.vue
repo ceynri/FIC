cd33f39518f0a35aac4706b5fb8ca2f23eefd80e
ceynri <ceynri@gmail.com>
2021-02-07 11:30:20 +0800

    feat(Compress): 完成图片列表功能

--- frontend/src/views/Compress.vue
+++ frontend/src/views/Compress.vue
@@ -2,12 +2,35 @@
   <div class="compression_page">
     <h1 class="title">Compressor</h1>
     <div class="comment">Upload facial image to compress</div>
-    <div class="upload_area clickable" ref="uploadArea" @click="selectFile">
-      <div class="tips">
-        <div>Click here to select image to upload</div>
-        <div>or drag & drop image here 😊</div>
+    <div class="container">
+      <div class="image_list" v-if="isAdded">
+        <div class="image_card" v-for="(item, i) in fileData" :key="i">
+          <div class="image_wrapper">
+            <img class="image" :src="item.data" />
+          </div>
+          <div class="image_name">{{ item.name }}</div>
+        </div>
       </div>
-      <input class="none" type="file" accept="image/*" multiple @change="getFile" ref="fileInput" />
+      <div
+        class="upload_area clickable"
+        :style="{ paddingTop: `${imageListHeight}px` }"
+        ref="uploadArea"
+        @click="selectFile"
+      >
+        <div class="tips" v-if="!isAdded">
+          <div class="tips_line">Click here to select image to upload</div>
+          <div class="tips_line">or drag & drop image here 😊</div>
+        </div>
+        <div class="tips" v-else>Add more...</div>
+        <input
+          class="none"
+          type="file"
+          accept="image/*"
+          multiple
+          @change="getFile"
+          ref="fileInput"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +41,14 @@ export default {
     return {
       fileData: [],
     };
+  },
+  computed: {
+    isAdded() {
+      return this.fileData.length > 0;
+    },
+    imageListHeight() {
+      return Math.min(350, this.fileData.length * 100);
+    },
   },
   mounted() {
     const dropArea = this.$refs.uploadArea;
@@ -34,7 +65,10 @@ export default {
       files.forEach((file) => {
         const fileReader = new FileReader();
         fileReader.addEventListener('load', () => {
-          this.fileData.push(fileReader.result);
+          this.fileData.push({
+            name: file.name,
+            data: fileReader.result,
+          });
           console.log(this.fileData);
         });
         fileReader.readAsDataURL(file);
@@ -72,22 +106,87 @@ export default {
     margin-bottom: 16px;
   }
 
-  .upload_area {
-    border: 1px var(--border2) dotted;
-    border-radius: 4px;
-    height: 450px;
+  .container {
+    $borderRadius: 4px;
 
-    .tips {
+    height: 450px;
+    width: 100%;
+    position: relative;
+
+    .upload_area {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 0;
+
       height: 100%;
+      width: 100%;
+
       display: flex;
-      flex-direction: column;
       justify-content: center;
       align-items: center;
-      font-size: 32px;
-      opacity: 0.3;
 
-      & > div {
-        margin-bottom: 12px;
+      border: var(--standard-border);
+      border-radius: $borderRadius;
+
+      .tips {
+        font-size: 32px;
+        opacity: 0.3;
+
+        .tips_line {
+          margin-bottom: 12px;
+        }
+      }
+    }
+
+    .image_list {
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: 1;
+
+      width: 100%;
+      max-height: 350px;
+      overflow-y: auto;
+      padding: 0 15px;
+
+      display: flex;
+      flex-direction: column;
+
+      background-color: #fff;
+      border-radius: $borderRadius;
+      box-shadow: 2px 4px 24px var(--shadow);
+
+      .image_card {
+        height: 100px;
+        padding: 15px 0;
+        display: flex;
+
+        border-bottom: var(--standard-border);
+
+        &:last-child {
+          border-bottom: none;
+        }
+
+        .image_wrapper {
+          height: 70px;
+          width: 70px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-right: 20px;
+
+          .image {
+            width: auto;
+            height: auto;
+            max-width: 100%;
+            max-height: 100%;
+          }
+        }
+
+        .image_name {
+          color: var(--text2);
+        }
       }
     }
   }
