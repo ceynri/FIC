4efcc8bb10dfd3211c882ccaaf5ff850f2793ee7
ceynri <ceynri@gmail.com>
2021-02-09 11:00:16 +0800

    refactor: page_frame改为为全局样式

--- frontend/src/views/Compress.vue
+++ frontend/src/views/Compress.vue
@@ -1,22 +1,18 @@
 <template>
-  <section class="compressor">
-    <PageFrame>
-      <header class="title_wrapper">
-        <h1 class="title">Compressor</h1>
-        <div class="comment">Upload facial images to compress</div>
-      </header>
-      <Uploader accept="image/*"
-    /></PageFrame>
+  <section class="compressor page_frame">
+    <header class="title_wrapper">
+      <h1 class="title">Compressor</h1>
+      <div class="comment">Upload facial images to compress</div>
+    </header>
+    <Uploader accept="image/*" />
   </section>
 </template>
 
 <script>
-import PageFrame from '@/components/PageFrame.vue';
 import Uploader from '@/components/Uploader.vue';
 
 export default {
   components: {
-    PageFrame,
     Uploader,
   },
 };
@@ -24,16 +20,5 @@ export default {
 
 <style lang="scss" scoped>
 .compressor {
-  .title {
-    font-size: 32px;
-    margin: 0;
-    margin-bottom: 8px;
-  }
-
-  .comment {
-    color: var(--text2);
-    font-size: 20px;
-    margin-bottom: 16px;
-  }
 }
 </style>
