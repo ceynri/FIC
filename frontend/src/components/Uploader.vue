7a78c08d6cb185babbcb0f4a966606c47a5d665e
ceynri <ceynri@gmail.com>
2021-02-19 22:58:43 +0800

    feat(frontend): axios异步请求上传文件FormData

--- frontend/src/components/Uploader.vue
+++ frontend/src/components/Uploader.vue
@@ -2,7 +2,7 @@
   <div class="uploader">
     <div class="container_border"></div>
     <overlay-scrollbars class="image_list" v-viewer="viewerOptions" ref="imageList">
-      <div class="image_item" v-for="(item, i) in fileData" :key="item.name">
+      <div class="image_item" v-for="(item, i) in fileList" :key="item.name">
         <div class="image_wrapper">
           <img class="image" v-if="isImage(item.dataUrl)" :src="item.dataUrl" />
           <IconBase v-else width="60px" height="60px" icon-name="archive">
@@ -65,6 +65,7 @@ import CancelIcon from '@/components/icons/CancelIcon.vue';
 import AddIcon from '@/components/icons/AddIcon.vue';
 
 import readFile from '@/utils/readFile';
+import { uploads } from '@/service';
 
 export default {
   props: {
@@ -79,7 +80,7 @@ export default {
   },
   data() {
     return {
-      fileData: [],
+      fileList: [],
       fileNameSet: new Set(),
       dragOver: false,
       viewer: null,
@@ -93,7 +94,7 @@ export default {
   },
   computed: {
     isAdded() {
-      return this.fileData.length > 0;
+      return this.fileList.length > 0;
     },
   },
   mounted() {
@@ -166,31 +167,50 @@ export default {
         this.fileNameSet.add(file.name);
 
         // 读取文件的dataUrl与二进制格式
-        const [dataUrl, blob] = await Promise.all([
-          readFile(file, 'dataUrl'),
-          readFile(file, 'blob'),
-        ]);
-        // 将图片保存为变量
-        const imageItem = {
-          dataUrl,
-          blob,
-          name: file.name,
-          size: file.size,
-        };
-        this.fileData.push(imageItem);
+        try {
+          const [dataUrl, blob] = await Promise.all([
+            readFile(file, 'dataUrl'),
+            readFile(file, 'blob'),
+          ]);
+          // 将图片保存为变量
+          const fileItem = {
+            dataUrl,
+            blob,
+            name: file.name,
+            size: file.size,
+            rawFile: file,
+          };
+          this.fileList.push(fileItem);
 
-        console.debug('fileData:', this.fileData);
+          // TODO: 将改为点击按钮后再执行上传文件并压缩的请求
+          // this.uploadFile(file);
 
-        // TODO: 下一行为Demo模块所做的单独逻辑，可考虑是否有优化空间
-        this.$emit('uploaded', imageItem);
+          console.debug('fileList:', this.fileList);
+          // TODO: 下一行为Demo模块所做的单独逻辑，可考虑是否有优化空间
+          this.$emit('uploaded', fileItem);
+        } catch (e) {
+          console.error(e);
+          alert(`${file.name}上传失败，请重新上传`);
+        }
       });
+    },
+    /**
+     * 上传文件
+     */
+    async uploadFile(file) {
+      try {
+        const res = await uploads(file);
+        console.log(res);
+      } catch (e) {
+        console.error(e);
+      }
     },
     /**
      * 删除特定的文件
      */
     deleteFile(i) {
-      this.fileNameSet.delete(this.fileData[i].name);
-      this.fileData.splice(i, 1);
+      this.fileNameSet.delete(this.fileList[i].name);
+      this.fileList.splice(i, 1);
     },
     /**
      * 查看大图
