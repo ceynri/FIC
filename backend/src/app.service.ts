45098b2dc7a42d118f3c7e6caf5ef3a47a4e76d1
ceynri <ceynri@gmail.com>
2021-02-24 14:25:05 +0800

    temp(demoProcess): 未完成的接口联调
    暂存，后续合并两个分支

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,46 +1,63 @@
 import { Injectable } from '@nestjs/common';
 
-import child_process from 'child_process';
+import * as child_process from 'child_process';
+import * as path from 'path';
 import * as fs from 'fs';
 
 import * as moment from 'moment';
 
 @Injectable()
 export class AppService {
+  readonly basePath = path.join(__dirname, '../public/temp');
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  uploadFile(file) {
-    console.log(file);
+  getPath(file): string {
     const date = moment().format('YYYY-MM-DD');
-    // TODO: 合理的存放位置
-    const storageAddress = `C:/Users/Haze/Desktop/毕设/temp/${date}/${file.originalname}`;
-    const writeStream = fs.createWriteStream(storageAddress);
-    writeStream.write(file.buffer);
+    return path.join(this.basePath,`./${date}/${file.originalname}`);
   }
 
-  // demoProcess(imageDto): Promise<any>  {
-  //   return new Promise((resolve, reject) => {
-  //     const workerProcess = child_process.exec(
-  //       `python demo.py ${imageDto}`,
-  //       (error, stdout, stderr) => {
-  //         if (error) {
-  //           console.log(error.stack);
-  //           console.log(`Error code ${error.code}`);
-  //           console.log(`Signal received ${error.signal}`);
-  //           reject(error);
-  //         }
-  //         console.log(`stdout: ${stdout}`);
-  //         console.log(`stderr: ${stderr}`);
-  //         resolve(stdout);
-  //       },
-  //     );
+  uploadFile(file, storagePath = '') {
+    if (!storagePath) {
+      storagePath = this.getPath(file);
+    }
+    console.debug(file);
+    const writeStream = fs.createWriteStream(storagePath);
+    writeStream.write(file.buffer);
+    return new Promise((resolve) => {
+      writeStream.on('drain', () => {
+        console.debug('done');
+        resolve(true);
+      })
+    })
+  }
+
+  async demoProcess(file) {
+    const path = this.getPath(file);
+    await this.uploadFile(file, path);
+    // TODO
+    return new Promise((resolve, reject) => {
+      const workerProcess = child_process.exec(
+        `python ./service/demo.py ${path}`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.log(error.stack);
+            console.log(`Error code ${error.code}`);
+            console.log(`Signal received ${error.signal}`);
+            reject(error);
+          }
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+          resolve(stdout);
+        },
+      );
   
-  //     workerProcess.on('exit', (code) => {
-  //       console.log(`子进程已退出，退出码 ${code}`);
-  //       resolve(code);
-  //     });
-  //   })
-  // }
+      workerProcess.on('exit', (code) => {
+        console.log(`子进程已退出，退出码 ${code}`);
+        resolve(code);
+      });
+    })
+  }
 }
