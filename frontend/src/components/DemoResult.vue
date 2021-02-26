af89337402df2d525dbb8e6b94b6fc197d7a2ecd
ceynri <ceynri@gmail.com>
2021-02-26 16:12:28 +0800

    feat(Demo): Demo拆分出Options、Result组件

---
+++ frontend/src/components/DemoResult.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo_result">
+    <div class="card">
+      <div class="image_wrapper">
+        <img class="image" :src="result.raw" alt="raw" ref="image" />
+      </div>
+      <div class="image_info">
+        <div class="image_name">raw image</div>
+        <div class="image_size">{{ result.rawResolution }}</div>
+        <div class="image_size">{{ result.rawSize }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    result: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
