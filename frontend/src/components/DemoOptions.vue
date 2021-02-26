b4f5614ae360b754740ddb36012387910e841473
ceynri <ceynri@gmail.com>
2021-02-26 16:12:28 +0800

    feat(Demo): Demo拆分出Options、Result组件

---
+++ frontend/src/components/DemoOptions.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="container">
+    <div class="preview_panel card">
+      <div class="image_wrapper">
+        <img class="image" :src="image.dataUrl" :alt="image.name" ref="image" />
+      </div>
+      <div class="image_info">
+        <div class="image_name">{{ image.name }}</div>
+        <div v-if="image.width" class="image_size">{{ image.width }} × {{ image.height }}</div>
+        <div class="image_size">{{ sizeFormat(image.size) }}</div>
+      </div>
+    </div>
+    <div class="setting_panel card">
+      <div class="setting_item">ratio</div>
+      <div class="setting_item">quality</div>
+      <div class="setting_item">xxx...</div>
+      <button class="next_btn clickable" @click="$emit('next')">
+        <div class="text">Process it</div>
+        <IconBase width="20px" height="20px" icon-name="next">
+          <RightArrowIcon />
+        </IconBase>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import RightArrowIcon from '@/components/icons/RightArrowIcon.vue';
+
+export default {
+  props: {
+    image: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {
+    RightArrowIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin: 30px 0;
+  display: flex;
+  align-items: flex-start;
+
+  .preview_panel {
+    width: 300px;
+    margin-right: 40px;
+    overflow: hidden;
+
+    .image_wrapper {
+      width: 100%;
+      max-height: 600px;
+      overflow: hidden;
+
+      .image {
+        display: block;
+        width: 100%;
+        height: auto;
+      }
+    }
+
+    .image_info {
+      color: var(--text2);
+      margin: 16px 20px 20px;
+
+      .image_name {
+        @include no-wrap;
+        color: var(--text);
+        font-size: 16px;
+        margin-bottom: 10px;
+      }
+
+      .image_size {
+        font-size: 12px;
+        margin-top: 8px;
+      }
+    }
+  }
+
+  .setting_panel {
+    flex: 1;
+    padding: 40px 40px 60px;
+
+    display: flex;
+    flex-direction: column;
+    // justify-content: space-evenly;
+
+    position: relative;
+
+    .setting_item {
+      font-size: 20px;
+      margin-bottom: 40px;
+    }
+    .next_btn {
+      position: absolute;
+      right: 40px;
+      bottom: 30px;
+
+      display: flex;
+      align-items: center;
+
+      color: var(--primary);
+      border-radius: 100px;
+      padding: 15px 20px;
+
+      transition: box-shadow var(--duration);
+
+      &:hover {
+        box-shadow: 2px 4px 32px -4px var(--shadow);
+      }
+
+      .text {
+        margin: 0 6px -0.1em;
+        font-size: 20px;
+      }
+    }
+  }
+}
+</style>
