4d39feb5fd96f6eb17154c27227e7b82e74a4fe6
ZXZhuang <2993968012@qq.com>
2021-03-02 17:50:23 +0800

    feat: add bottleneck_entropy

--- algorithm/dataset.py
+++ algorithm/dataset.py
@@ -5,9 +5,44 @@ from PIL import Image
 from pathlib import Path
 from autocrop import Cropper
 from rich.progress import track
-from p_tqdm import p_umap
 
+def collate(batch):
+    batch = list(filter(lambda x: x is not None, batch))
+    while len(batch) < 16:
+        j = len(batch)
+        for i in (0, j):
+            if len(batch) < 16:
+                batch.append(batch[i])
+    return default_collate(batch)
 
+class dataset(Dataset):
+    def __init__(self, path):
+        self.path = Path(path)
+        self.image_files = []
+        self.crop = Cropper()
+        self.transform = transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor()
+        ])
+
+        print("begin loading")
+        for pdir in track(list(self.path.glob('*/*.jpg'))):
+            # use path index image
+            self.image_files.append(str(pdir))
+        print('finish loading')
+
+    def __len__(self):
+        return (len(self.image_files))
+
+    def __getitem__(self, index):
+        cropped = self.crop.crop(self.image_files[index])
+        if cropped is None:
+            return None
+        img = Image.fromarray(cropped).convert('RGB')
+        img = self.transform(img)
+        return img
+
+'''
 class dataset(Dataset):
     def __init__(self):
         self.path = Path('../data/train')
@@ -68,7 +103,7 @@ class dataset(Dataset):
 
     def __len__(self):
         return (len(self.image_files))
-
+'''
 
 if __name__ == '__main__':
-    dataset()
+    dataset('./data/train')
