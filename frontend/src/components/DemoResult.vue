f4e1e17099edd4bac6c5062e4e978bbca3d81f35
ceynri <ceynri@gmail.com>
2021-03-14 10:19:49 +0800

    feat: 完善demo结果展示，支持hover切换图片效果

--- frontend/src/components/DemoResult.vue
+++ frontend/src/components/DemoResult.vue
@@ -1,38 +1,80 @@
 <template>
   <div class="demo_result">
-    <div class="card">
-      <div class="image_wrapper">
-        <img class="image" :src="data.input" alt="input" ref="image" />
+    <div class="contrast_section">
+      <div
+        class="contrast_image_wrapper shadow"
+        @mouseover="changeImage('in')"
+        @mouseleave="changeImage('out')"
+      >
+        <div class="contrast_image card">
+          <img class="image" :src="image.input" alt="input image" />
+          <div class="image_info">
+            <div class="image_name">original image</div>
+            <div class="image_size">{{ sizeFormat(data.size.input) }}</div>
+          </div>
+        </div>
+        <div class="contrast_image card" :class="{ hidden }">
+          <img class="image" :src="contrastImage.src" :alt="contrastImage.name" />
+          <div class="image_info">
+            <div class="image_name">{{ contrastImage.name }} image</div>
+            <div class="image_size">{{ sizeFormat(contrastImage.size) }}</div>
+          </div>
+        </div>
       </div>
-      <div class="image_info">
-        <div class="image_name">input image</div>
-        <div class="image_data_info">10KB</div>
+      <div class="contrast_info card shadow">
+        <div class="info_line clickable" @click="select('output')">
+          <div class="selector" :class="{ selected: selectedImageName == 'output' }"></div>
+          <div class="image_name_wrapper">
+            <div class="image_name">Our method</div>
+          </div>
+          <div class="image_info">
+            <div>{{ sizeFormat(data.size.fic) }} (compress data)</div>
+            <div>compression ratio: {{ percentFormat(data.eval.fic_compression_ratio) }}</div>
+            <div>PSNR: {{ data.eval.fic_psnr.toFixed(6) }}</div>
+            <div>SSIM: {{ data.eval.fic_ssim.toFixed(6) }}</div>
+          </div>
+        </div>
+        <div class="divider"></div>
+        <div class="info_line clickable" @click="select('jpeg')">
+          <div class="selector" :class="{ selected: selectedImageName == 'jpeg' }"></div>
+          <div class="image_name_wrapper">
+            <div class="image_name">JPEG</div>
+          </div>
+          <div class="image_info">
+            <div>{{ sizeFormat(data.size.jpeg) }}</div>
+            <div>compression ratio: {{ percentFormat(data.eval.jpeg_compression_ratio) }}</div>
+            <div>PSNR: {{ data.eval.jpeg_psnr.toFixed(6) }}</div>
+            <div>SSIM: {{ data.eval.jpeg_ssim.toFixed(6) }}</div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="card">
-      <div class="image_wrapper">
-        <img class="image" :src="data.output" alt="output" ref="image" />
-      </div>
-      <div class="image_info">
-        <div class="image_name">output image</div>
-        <div class="image_data_info">3KB (Compressed data)</div>
-        <div class="image_data_info">0.8 bpp</div>
-      </div>
+    <div class="card_wrapper">
+      <!-- TODO 提供下载/查看原图功能 -->
+      <ImageCard class="card clickable" :src="image.input" name="input"></ImageCard>
+      <ImageCard class="card clickable" :src="image.feat" name="feature"></ImageCard>
+      <ImageCard class="card clickable" :src="image.output" name="output"></ImageCard>
+      <ImageCard class="card clickable" :src="image.jpeg" name="jpeg"></ImageCard>
+      <ImageCard class="card clickable" :src="image.resi" name="residual"></ImageCard>
+      <ImageCard class="card clickable" :src="image.recon" name="reconstruction"></ImageCard>
+      <ImageCard
+        class="card clickable"
+        :src="image.resi_norm"
+        name="normalize residual"
+      ></ImageCard>
+      <ImageCard
+        class="card clickable"
+        :src="image.recon_norm"
+        name="normalize reconstruction"
+      ></ImageCard>
+      <ImageCard class="card clickable" name="compress data" src="/assets/archive.png"></ImageCard>
     </div>
-    <!-- <div class="card">
-      <div class="image_wrapper">
-        <img class="image" :src="data.input" alt="jpeg compress" ref="image" />
-      </div>
-      <div class="image_info">
-        <div class="image_name">JPEG image</div>
-        <div class="image_data_info">8KB</div>
-        <div class="image_data_info">0.8 bpp</div>
-      </div>
-    </div> -->
   </div>
 </template>
 
 <script>
+import ImageCard from '@/components/ImageCard.vue';
+
 export default {
   props: {
     data: {
@@ -40,43 +82,137 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      selectedImageName: 'output',
+      hidden: false,
+    };
+  },
+  computed: {
+    image() {
+      return this.data.image;
+    },
+    contrastImage() {
+      return {
+        name: this.selectedImageName,
+        src: this.image[this.selectedImageName],
+        size: this.data.size[this.selectedImageName],
+      };
+    },
+  },
+  methods: {
+    changeImage(type) {
+      this.hidden = type === 'in';
+    },
+    select(name) {
+      this.selectedImageName = name;
+    },
+  },
+  components: {
+    ImageCard,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .demo_result {
-  padding: 40px 0;
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
+  $margin: 20px;
+  margin: 0 auto;
 
-  .card {
-    margin: 10px;
-    .image_wrapper {
-      .image {
-        width: auto;
-        height: auto;
-        max-width: 100%;
-        max-height: 100%;
+  .contrast_section {
+    display: grid;
+    grid-template-columns: 38.2% 61.8%;
+    margin: $margin 0;
+
+    .contrast_image_wrapper {
+      max-width: 100%;
+      max-height: 100%;
+      margin-right: $margin;
+
+      position: relative;
+
+      .contrast_image {
+        transition: opacity var(--duration);
+        width: 100%;
+
+        .image {
+          width: 100%;
+          height: 100%;
+        }
+
+        &.hidden {
+          opacity: 0;
+        }
+
+        &:nth-of-type(2) {
+          position: absolute;
+          top: 0;
+          left: 0;
+        }
+      }
+
+      .image_info {
+        display: flex;
+        justify-content: space-between;
+        margin: 12px;
       }
     }
 
-    .image_info {
-      color: var(--text2);
-      margin: 16px 20px 20px;
+    .contrast_info {
+      margin-left: $margin;
+      padding: 50px 60px;
 
-      .image_name {
-        @include no-wrap;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      .info_line {
+        display: flex;
+      }
+
+      .selector {
+        height: 14px;
+        width: 14px;
+        margin: 8px 20px 8px 0;
+        border: 1px dotted var(--border2);
+        // mix-blend-mode: exclusion;
+        border-radius: 50%;
+        box-sizing: border-box;
+        transition: all var(--duration);
+        flex: none;
+
+        &.selected {
+          border: none;
+          background: var(--primary);
+        }
+      }
+
+      .image_name_wrapper {
         color: var(--text);
-        font-size: 16px;
-        margin-bottom: 10px;
+        font-size: 22px;
+        line-height: 1.2;
+        position: relative;
+        flex: 1.2;
       }
 
-      .image_data_info {
-        font-size: 12px;
-        margin-top: 8px;
+      .image_info {
+        font-size: 16px;
+        line-height: 1.5;
+        color: var(--text2);
+        flex: 2;
+      }
+
+      .divider {
+        border-top: var(--border) solid 1px;
       }
     }
+  }
+
+  .card_wrapper {
+    padding: 40px 0;
+    display: grid;
+    grid-template-columns: repeat(4, 22.5%);
+    gap: 3.33%;
   }
 }
 </style>
