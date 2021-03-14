b422f35a5749abc029402113ea06dde14e94b8e9
ceynri <ceynri@gmail.com>
2021-03-14 10:17:41 +0800

    feat: 将Uploader接收到的文件列表改为v-model

--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = 'http://127.0.0.1/api';
+// const baseUrl = 'http://127.0.0.1/api';
+// TODO 检查该ip
+const baseUrl = 'http://172.30.45.58/api';
 
 // eslint-disable-next-line import/prefer-default-export
 export function uploads(file) {
@@ -13,12 +15,26 @@ export function uploads(file) {
   });
 }
 
-export function demoProcess(file) {
+export async function demoProcess(file) {
   const data = new FormData();
   data.append('file', file);
-  return axios.post(`${baseUrl}/demo_process`, data, {
+  const res = await axios.post(`${baseUrl}/demo_process`, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
   });
+  return res.data;
+}
+
+export async function compress(files) {
+  const data = new FormData();
+  files.forEach((file) => {
+    data.append('files', file);
+  });
+  const res = await axios.post(`${baseUrl}/compress`, data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return res.data;
 }
