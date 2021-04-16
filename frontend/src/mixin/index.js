96028ba31d1fe6dc87d99c3be7a88ba9e36f83a0
ceynri <ceynri@gmail.com>
2021-04-16 17:04:29 +0800

    feat(frontend): 删除弃用的cssVars

--- frontend/src/mixin/index.js
+++ frontend/src/mixin/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
-import { mapState, mapMutations } from 'vuex';
 
 import IconBase from '@/components/IconBase.vue';
 import { sizeFormat, percentFormat } from '@/utils/formatter';
 
 const mixins = {
-  computed: {
-    ...mapState(['cssVars']),
-  },
   methods: {
-    ...mapMutations(['setState']),
     sizeFormat,
     percentFormat,
   },
