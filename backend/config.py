bb6ed67c86a55a2ef3abb0dcbb05ef77e9bc93d8
ceynri <ceynri@gmail.com>
2021-04-17 21:21:08 +0800

    feat: 配置抽取为config.py

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+recon_param_path = './params/recon/30w/baseLayer_5.pth'
+e_param_path = './params/e_layer/enhanceLayer_2.pth'
+
+base_path = './public/result'
+base_url = '/assets/result/'
+
+IMAGE_PIXEL_NUM = 256 * 256
