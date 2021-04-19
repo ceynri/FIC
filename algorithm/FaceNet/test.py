61ff965783a5f5e97163aa83916016f77f53df4a
ceynri <ceynri@gmail.com>
2021-04-19 15:27:56 +0800

    feat: 代码更新，代码格式化，添加tensorboard

--- algorithm/FaceNet/test.py
+++ algorithm/FaceNet/test.py
@@ -8,20 +8,9 @@ from facenet_pytorch import InceptionResnetV1
 from DeepRcon import DRcon
 # from bit_estimate import BitEstimator
 
-def reload():
-    #转旧版本pth
-    pth = './enhancement_epoch_9.pth'
-    checkpoint = torch.load(pth)
-    codec = ImageCompressor().eval()
-    codec = codec.cuda()
-    codec = nn.DataParallel(codec).cuda()
-    codec.load_state_dict(checkpoint)
-    torch.save(codec.state_dict(), "./enhanceLayer.pth", _use_new_zipfile_serialization=False)
-
 if __name__ == "__main__":
-    #reload()
     resnet = InceptionResnetV1(pretrained='vggface2').eval().cuda()
-    dataset = dataset()
+    dataset = dataset('/data/chenyangrui/train')
     dl = DataLoader(
         dataset=dataset,
         num_workers=0,
@@ -35,16 +24,16 @@ if __name__ == "__main__":
     Base = DRcon().eval()
     Base = Base.cuda()
     Base = nn.DataParallel(Base).cuda()
-    B_param = torch.load('../save/baseLayer.pth')
+    B_param = torch.load('/data/chenyangrui/cyr/baseLayer_6.pth')
     Base.load_state_dict(B_param)
 
     # EnhancementLayer
-    codec = ImageCompressor().eval()
-    codec = codec.cuda()
-    codec = nn.DataParallel(codec).cuda()
-    E_param = torch.load('../save/enhanceLayer.pth')
-    codec.load_state_dict(E_param)
-    a = 0
+    # codec = ImageCompressor().eval()
+    # codec = codec.cuda()
+    # codec = nn.DataParallel(codec).cuda()
+    # E_param = torch.load('../save/enhanceLayer.pth')
+    # codec.load_state_dict(E_param)
+    # a = 0
 
     with torch.no_grad():
         for data in dl:
@@ -59,22 +48,22 @@ if __name__ == "__main__":
             feat = feat.cuda()
             x_feat = Base(feat)
 
-            x_resi = x - x_feat
-            t_Max = torch.max(x_resi)
-            t_Min = torch.min(x_resi)
-            x_tex = (x_resi - t_Min) / (t_Max - t_Min)  # min-max normalization
-            x_tex.cuda()
-            x_resi.cuda()
-            decoded = codec(x_resi)
-            r_Max = torch.max(decoded)
-            r_Min = torch.min(decoded)
-            x_rec = (decoded - r_Min) / (r_Max - r_Min)
-            x_rec.cuda()
-            output = x_feat + decoded
-            torchvision.utils.save_image(x, '../result/' + str(a) + '.png')
-            torchvision.utils.save_image(x_tex, '../result/' + str(a) + '_tex.png')
-            torchvision.utils.save_image(output, '../result/' + str(a) + '_output.png')
-            torchvision.utils.save_image(x_feat, '../result/' + str(a) + '_feat.png')
-            torchvision.utils.save_image(x_rec, '../result/' + str(a) + '_rec.png')
-            torchvision.utils.save_image(decoded, '../result/' + str(a) + '_decoded.png')
+            # x_resi = x - x_feat
+            # t_Max = torch.max(x_resi)
+            # t_Min = torch.min(x_resi)
+            # x_tex = (x_resi - t_Min) / (t_Max - t_Min)  # min-max normalization
+            # x_tex.cuda()
+            # x_resi.cuda()
+            # decoded = codec(x_resi)
+            # r_Max = torch.max(decoded)
+            # r_Min = torch.min(decoded)
+            # x_rec = (decoded - r_Min) / (r_Max - r_Min)
+            # x_rec.cuda()
+            # output = x_feat + decoded
+            torchvision.utils.save_image(x, f'../result/{a}.png')
+            # torchvision.utils.save_image(x_tex, f'../result/{a}_tex.png')
+            # torchvision.utils.save_image(output, f'../result/{a}_output.png')
+            torchvision.utils.save_image(x_feat, f'../result/{a}_feat.png')
+            # torchvision.utils.save_image(x_rec, f'../result/{a}_rec.png')
+            # torchvision.utils.save_image(decoded, f'../result/{a}_decoded.png')
             a = a + 1
--- algorithm/FaceNet/train_enhance.py
+++ algorithm/FaceNet/train_enhance.py
@@ -8,16 +8,18 @@ from RateDistortionLoss import RateDistortionLoss
 from facenet_pytorch import InceptionResnetV1
 from DeepRcon import DRcon
 import cv2
+from torch.utils.tensorboard import SummaryWriter
+
 
 def configure_optimizers(net, lr, aux_lr):
-    """Separate parameters for the main optimizer and the auxiliary optimizer.
-    Return two optimizers"""
+    '''Separate parameters for the main optimizer and the auxiliary optimizer.
+    Return two optimizers'''
 
     parameters = set(
-        p for n, p in net.named_parameters() if not n.endswith(".quantiles")
+        p for n, p in net.named_parameters() if not n.endswith('.quantiles')
     )
     aux_parameters = set(
-        p for n, p in net.named_parameters() if n.endswith(".quantiles")
+        p for n, p in net.named_parameters() if n.endswith('.quantiles')
     )
 
     # Make sure we don't have an intersection of parameters
@@ -38,8 +40,9 @@ def configure_optimizers(net, lr, aux_lr):
     )
     return optimizer, aux_optimizer
 
+
 class CustomDataParallel(nn.DataParallel):
-    """Custom DataParallel to access the module methods."""
+    '''Custom DataParallel to access the module methods.'''
 
     def __getattr__(self, key):
         try:
@@ -47,9 +50,8 @@ class CustomDataParallel(nn.DataParallel):
         except AttributeError:
             return getattr(self.module, key)
 
+
 if __name__ == '__main__':
-    # isload = True
-    # gpu_num = torch.cuda.device_count()
     cv2.ocl.setUseOpenCL(False)
     ds = dataset('/data/chenyangrui/train')
     dl = DataLoader(
@@ -63,46 +65,47 @@ if __name__ == '__main__':
     )
 
     # FaceNet for extracting features
-    resnet = InceptionResnetV1(pretrained='vggface2').eval().cuda()
-    #resnet = nn.DataParallel(resnet, list(range(gpu_num)))
-    resnet = nn.DataParallel(resnet, [0,2])
+    resnet = InceptionResnetV1(pretrained='vggface2').eval().cuda(5)
+    resnet = nn.DataParallel(resnet, [5, 6])
 
     # reconstruct face by feature
-    Base = DRcon().eval()
-    Base = Base.cuda()
-    Base = nn.DataParallel(Base, [0,2])
-    param = torch.load('/data/chenyangrui/save/base_layer.pth')
+    Base = DRcon().cuda(5)
+    Base = nn.DataParallel(Base, [5, 6])
+    # param = torch.load('/data/chenyangrui/save/base_layer.pth')
+    param = torch.load('/data/chenyangrui/cyr/30w/baseLayer_7.pth', map_location='cuda:1')
     Base.load_state_dict(param)
 
-    codec = AutoEncoder().cuda()
-    codec = CustomDataParallel(codec, [0,2])
+    codec = AutoEncoder().cuda(5)
+    codec = CustomDataParallel(codec, [5, 6])
     optimizer, aux_optimizer = configure_optimizers(codec, lr=1e-3, aux_lr=1e-3)
     criterion = RateDistortionLoss(lmbda=1024)
     clip_max_norm = 1.0
     out_criterion = {}
     epoch = 0
 
+    writer = SummaryWriter(comment='_enhance_1024')
+
     # if isload:
-    #     checkpoint = torch.load("/data/chenyangrui/save/enhanceLayer_checkpoints")
+    #     checkpoint = torch.load('/data/chenyangrui/save/enhanceLayer_checkpoints')
     #     codec.load_state_dict(checkpoint['model_state_dict'])
-    #     optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    #     aux_optimizer.load_state_dict(checkpoint["aux_optimizer_state_dict"])
-    #     epoch = checkpoint["epoch"]
+    #     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    #     aux_optimizer.load_state_dict(checkpoint['aux_optimizer_state_dict'])
+    #     epoch = checkpoint['epoch']
 
-
-    print("start train")
+    print('start train')
+    global_step = 0
     while(epoch < 10):
         codec.train()
         for i, d in enumerate(dl):
             # BaseLayer
             label = d
-            label = label.cuda()
+            label = label.cuda(5)
             feat = resnet(label)
             feat = torch.squeeze(feat, 1)
             feat = torch.unsqueeze(feat, 2)
             feat = torch.unsqueeze(feat, 3)
             # feat's shape is [N,512,1,1]
-            feat = feat.cuda()
+            feat = feat.cuda(5)
             data = Base(feat)
 
             aux_optimizer.zero_grad()
@@ -113,13 +116,13 @@ if __name__ == '__main__':
             Max = torch.max(resi)
             Min = torch.min(resi)
             tex_norm = (resi - Min) / (Max - Min)  # min-max normalization
-            tex_norm = tex_norm.cuda()
+            tex_norm = tex_norm.cuda(5)
             decoded = codec(tex_norm)
-            x_hat = decoded["x_hat"]
+            x_hat = decoded['x_hat']
             recon = (Max - Min) * x_hat + Min
 
             out_criterion = criterion(decoded, resi, tex_norm, x_hat)
-            out_criterion["loss"].backward()
+            out_criterion['loss'].backward()
 
             if clip_max_norm > 0:
                 torch.nn.utils.clip_grad_norm_(codec.parameters(), clip_max_norm)
@@ -128,15 +131,27 @@ if __name__ == '__main__':
             aux_loss = codec.aux_loss()
             aux_loss.backward()
             aux_optimizer.step()
+            
+            # 记录loss
+            loss = out_criterion["loss"].item()
+            mse_loss = out_criterion["mse_loss"].item()
+            bpp_loss = out_criterion["bpp_loss"].item()
+            aux_loss = aux_loss.item()
+            writer.add_scalar('loss/total', loss, global_step)
+            writer.add_scalar('loss/mse', mse_loss, global_step)
+            writer.add_scalar('loss/bpp', bpp_loss, global_step)
+            writer.add_scalar('loss/aux', aux_loss, global_step)
+            global_step += 1
 
-            if i%10==0:
+            if i % 10 == 0:
                 print(
-                    f"Train epoch, iter {epoch}, {i}: "
-                    f'\tLoss: {out_criterion["loss"].item():.3f} |'
-                    f'\tMSE loss: {out_criterion["mse_loss"].item():.3f} |'
-                    f'\tBpp loss: {out_criterion["bpp_loss"].item():.2f} |'
-                    f"\tAux loss: {aux_loss.item():.2f}"
+                    f'Epoch {epoch}, batch {i}\t| '
+                    f'Loss: {loss:.3f}\t| '
+                    f'MSE loss: {mse_loss:.5f}\t| '
+                    f'Bpp loss: {bpp_loss:.4f}\t| '
+                    f'Aux loss: {aux_loss:.2f}'
                 )
+
         epoch += 1
         torch.save({
             'epoch': epoch,
@@ -144,5 +159,5 @@ if __name__ == '__main__':
             'optimizer_state_dict': optimizer.state_dict(),
             'aux_optimizer_state_dict': aux_optimizer.state_dict(),
             'loss': out_criterion
-        }, '/data/chenyangrui/save/enhanceLayer_checkpoints')
-        torch.save(codec.state_dict(), "/data/chenyangrui/save/enhanceLayer_lamda.pth")
+        }, '/data/chenyangrui/save/1024/enhanceLayer_checkpoints')
+        torch.save(codec.state_dict(), f'/data/chenyangrui/cyr/1024/enhanceLayer_{epoch}.pth')
