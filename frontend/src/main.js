041bb9b577ed9cf8fae6f419a09896dd4a5bafc5
ceynri <ceynri@gmail.com>
2021-04-25 12:19:05 +0800

    chore: 删除未用到的VueX依赖

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 
 import router from './router';
-import store from './store';
 import './mixin';
 import './plugins';
 
@@ -13,6 +12,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app');
