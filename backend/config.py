3ebfcf42f9936cb8e165557a15df772eb2632e8a
ceynri <ceynri@gmail.com>
2021-04-25 16:38:37 +0800

    chore: 参数路径规范化

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,5 @@
 # 参数文件路径
-RECON_PARAM_PATH = './params/deconv_recon/30w/baseLayer_7.pth'
-E_LAYER_PARAM_PATH = './params/gdn_model/5120/gdnmodel_10.pth'
+RECON_PARAM_PATH = './params/deconv_recon/deconv_recon_30w.pth'
 
 # 图片保存位置
 BASE_PATH = './public/result'
@@ -16,9 +15,7 @@ IMAGE_PIXEL_NUM = IMAGE_SIZE * IMAGE_SIZE
 
 # enhancement layer 参数文件路径映射表
 E_PARAM_MAP = {
-    'low': './params/gdn_model/1024/gdnmodel_10.pth',
-    # 'medium': './params/gdn_model/2560/gdnmodel_6.pth',
-    'medium': './params/gdn_model/3600/gdnmodel_6.pth',
-    # 'semi-high': './params/gdn_model/3600/gdnmodel_6.pth',
-    'high': './params/gdn_model/5120/gdnmodel_10.pth',
+    'low': './params/gdn_model/gdn_model_1024.pth',
+    'medium': './params/gdn_model/gdn_model_2560.pth',
+    'high': './params/gdn_model/gdn_model_5120.pth',
 }
